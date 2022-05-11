3bd7f888c9d51d8b43c5508aea4411b95d0b03f4
Brandon Palmer <brandonpalmerqwerty@gmail.com>
2022-05-11 11:09:52 +0100

    Ecommerce 1.1

--- pages/success.js
+++ pages/success.js
@@ -5,7 +5,7 @@ import Link from 'next/link'
 import { runFireworks } from '../utils/utils'
 
 
-const success = () => {
+const Success = () => {
     const { setCartItems, setTotalPrice, setTotalQuantity } = useStateContext();
 
     useEffect(() => {
@@ -40,4 +40,4 @@ const success = () => {
     )
 }
 
-export default success
+export default Success
